8c9479a8dced84edefbc2085cea4fccc43f147e4
DJWilde <patryk.kaczor2609@gmail.com>
2021-09-02 19:09:54 +0200

    Rozpoczęto implementację kreatora wykresów

---
+++ src/main/java/com/djwilde/inzynierka/config/GnuplotConstants.java
@@ -0,0 +1,10 @@
+package com.djwilde.inzynierka.config;
+
+public class GnuplotConstants {
+    public static final String[] GNUPLOT_KEYWORDS = new String[] {
+            "break", "cd", "call", "clear", "continue", "do", "evaluate", "exit", "fit", "help", "history", "if", "for",
+            "import", "load", "pause", "plot", "print", "printerr", "pwd", "quit", "raise", "refresh", "replot", "reread",
+            "reset", "save", "set", "shell", "splot", "stats", "system", "test", "toggle", "undefine", "unset", "update",
+            "while"
+    };
+}
--- src/main/java/com/djwilde/inzynierka/newplotwindow/NewPlotWindowController.java
+++ src/main/java/com/djwilde/inzynierka/newplotwindow/NewPlotWindowController.java
@@ -1,4 +1,72 @@
 package com.djwilde.inzynierka.newplotwindow;
 
+import com.djwilde.inzynierka.config.GnuplotConstants;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import org.fxmisc.richtext.CodeArea;
+
+import java.util.List;
+
 public class NewPlotWindowController {
+    @FXML
+    private BorderPane newPlotBorderPane;
+
+    @FXML
+    private TextField plotTitleTextField;
+    @FXML
+    private CheckBox xCheckBox;
+    @FXML
+    private CheckBox yCheckBox;
+    @FXML
+    private CheckBox zCheckBox;
+    @FXML
+    private CheckBox legendCheckBox;
+    @FXML
+    private ListView<String> keywordListView;
+    @FXML
+    private Button addKeywordToScriptButton;
+    @FXML
+    private Button editScriptButton;
+    @FXML
+    private CodeArea scriptTextArea;
+    @FXML
+    private Button executeButton;
+    @FXML
+    private Button saveScriptButton;
+    @FXML
+    private Button cancelButton;
+
+    public void initialize() {
+        keywordListView.setItems(FXCollections.observableList(List.of(GnuplotConstants.GNUPLOT_KEYWORDS)));
+        scriptTextArea.setDisable(true);
+
+        addKeywordToScriptButton.setOnAction(actionEvent -> addKeywordToScript());
+        editScriptButton.setOnAction(actionEvent -> editScript());
+        executeButton.setOnAction(actionEvent -> executeScript());
+        saveScriptButton.setOnAction(actionEvent -> saveScript());
+        cancelButton.setOnAction(actionEvent -> cancel());
+    }
+
+    public void addKeywordToScript() {
+        String selectedKeyword = keywordListView.getSelectionModel().getSelectedItem();
+        scriptTextArea.appendText(selectedKeyword + "\n");
+    }
+
+    public void editScript() {
+        scriptTextArea.setDisable(!scriptTextArea.isDisabled());
+    }
+
+    public void executeScript() {
+
+    }
+
+    public void saveScript() {
+
+    }
+
+    public void cancel() {
+
+    }
 }
